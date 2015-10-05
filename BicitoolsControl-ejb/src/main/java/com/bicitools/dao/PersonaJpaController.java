b710536a195ef0350a647ce99de978f3b78a35b0
Jhony <jm.toro10@uniandes.edu.co>
2015-10-05 00:06:47 -0500

    ajustes iniciales

---
+++ BicitoolsControl-ejb/src/main/java/com/bicitools/dao/PersonaJpaController.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bicitools.dao;
+
+import com.bicitools.dao.exceptions.NonexistentEntityException;
+import com.bicitools.dao.exceptions.RollbackFailureException;
+import com.bicitools.entity.Persona;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author Jhony
+ */
+public class PersonaJpaController implements Serializable {
+
+    public PersonaJpaController(EntityManagerFactory emf) {
+        
+        this.emf = emf;
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Persona persona) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            
+            em = getEntityManager();
+            //em.getTransaction().begin();
+            em.persist(persona);
+            //em.getTransaction().commit();
+            
+        } catch (Exception ex) {
+          System.out.println("error creando persona ex: "+ex.getMessage());
+            
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Persona persona) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            
+            em = getEntityManager();
+            persona = em.merge(persona);
+            
+        } catch (Exception ex) {
+            try {
+            
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = persona.getId();
+                if (findPersona(id) == null) {
+                    throw new NonexistentEntityException("The persona with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            
+            em = getEntityManager();
+            Persona persona;
+            try {
+                persona = em.getReference(Persona.class, id);
+                persona.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The persona with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(persona);
+            
+        } catch (Exception ex) {
+            try {
+            
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Persona> findPersonaEntities() {
+        return findPersonaEntities(true, -1, -1);
+    }
+
+    public List<Persona> findPersonaEntities(int maxResults, int firstResult) {
+        return findPersonaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Persona> findPersonaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Persona.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Persona findPersona(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Persona.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getPersonaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Persona> rt = cq.from(Persona.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
