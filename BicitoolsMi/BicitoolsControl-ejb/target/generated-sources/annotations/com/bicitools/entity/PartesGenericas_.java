84cdb05cef1ffabbee5230e325b2c257b0895dec
jhonyt37 <jm.toro10@uniandes.edu.co>
2015-11-22 18:39:50 -0500

    compilando proyecto reportes y mibici
    compilando proyecto reportes y mibici

--- BicitoolsMi/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/PartesGenericas_.java
+++ BicitoolsMi/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/PartesGenericas_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-08T02:59:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T18:37:05")
 @StaticMetamodel(PartesGenericas.class)
 public class PartesGenericas_ { 
 
