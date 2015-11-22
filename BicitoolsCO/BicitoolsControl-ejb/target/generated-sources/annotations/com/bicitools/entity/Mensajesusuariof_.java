3774efb48c43233bee2350a3282b56aebd0a0e02
Ludwing Badillo Dominguez <ludwingbd79@gmail.com>
2015-11-22 17:42:48 -0500

    cambios do
    cambios para el comunicaciones y domicilios para generacipn de paginas

--- BicitoolsCO/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Mensajesusuariof_.java
+++ BicitoolsCO/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Mensajesusuariof_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-01T10:32:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T17:25:46")
 @StaticMetamodel(Mensajesusuariof.class)
 public class Mensajesusuariof_ { 
 
--- BicitoolsDO/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Horariodomiciliario_.java
+++ BicitoolsDO/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Horariodomiciliario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-27T19:44:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T17:25:45")
 @StaticMetamodel(Horariodomiciliario.class)
 public class Horariodomiciliario_ { 
 
