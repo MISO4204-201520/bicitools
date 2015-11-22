320067e5c3cd78319ae8a946de0483bb6a1f1786
jhonyt37 <jm.toro10@uniandes.edu.co>
2015-11-22 18:43:33 -0500

    Agregando funcionalidad de variabilidad completa
    Agregando funcionalidad de variabilidad completa

--- Variabilidad/var-reportes/src/var/reportes/VarReportes.java
+++ Variabilidad/var-reportes/src/var/reportes/VarReportes.java
@@ -5,6 +5,10 @@
  */
 package var.reportes;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  *
  * @author jhony
@@ -14,8 +18,53 @@ public class VarReportes {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+        public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        //String[] data = new String[3];
+        //data[0]="historialdeviajes,reportes,mas cosas,features";
+        System.out.println("salida");
+        File dir = new File("../../bicitoolsRE/BicitoolsCommon-jar/src/main/resources/prop/");
+        dir.mkdirs();
+        File tmp = new File(dir, "general.properties");
+        tmp.createNewFile();
+
+        String content = "#This is the text content";
+
+        try (FileOutputStream fop = new FileOutputStream(tmp)) {
+
+            // if file doesnt exists, then create it
+            if (!tmp.exists()) {
+                tmp.createNewFile();
+            }
+
+            for (int i = 0; i < args.length; i++) {
+                // get the content in bytes
+                if (args[i] != null) {
+
+                    content = args[i];
+                    String[] features = content.split(",");
+                    for (String feature : features) {
+                        if (feature.equals("historialdeviajes") || feature.equals("reportes")) {
+                            feature += " = true";
+                            byte[] contentInBytes = feature.getBytes();
+
+                            fop.write(contentInBytes);
+                            fop.write('\n');
+                            fop.flush();
+
+                            System.out.println("Done");
+                        }
+
+                    }
+
+                }
+
+            }
+            fop.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
