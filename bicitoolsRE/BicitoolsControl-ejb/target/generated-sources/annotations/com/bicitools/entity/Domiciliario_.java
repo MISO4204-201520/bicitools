02a9e8a32823eb12b43f82cd9087c1a0e70f6235
jhonyt37 <jm.toro10@uniandes.edu.co>
2015-11-22 18:13:17 -0500

    Subiendo variables a reporte para variabilidad
    Subiendo variables a reporte para variabilidad

--- bicitoolsRE/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Domiciliario_.java
+++ bicitoolsRE/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/Domiciliario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T15:53:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T18:11:32")
 @StaticMetamodel(Domiciliario.class)
 public class Domiciliario_ { 
 
