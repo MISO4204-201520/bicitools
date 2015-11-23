33f39541ff9f7fca1929295f93e1d2afcdb131ff
jhonyt37 <jm.toro10@uniandes.edu.co>
2015-11-22 19:03:48 -0500

    ignorando archivos de compilacion
    ignorando archivos de compilacion

--- bicitoolsRE/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/EstadoOrden_.java
+++ bicitoolsRE/BicitoolsControl-ejb/target/generated-sources/annotations/com/bicitools/entity/EstadoOrden_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T18:34:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-11-22T19:02:36")
 @StaticMetamodel(EstadoOrden.class)
 public class EstadoOrden_ { 
 
